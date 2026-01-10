95bbbebc9a8efb30ef42107783ad79816d77cdb5
Zero <120384602+CallmeZewo@users.noreply.github.com>
2026-01-10 17:10:06 +0100

    Added ElementClass script

---
+++ Skylander Selection/Assets/ElementClass.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using static Enums;
+
+public class ElementClass
+{
+    public Sprite Sprite;
+    public ElementEnum Element;
+
+    public ElementClass(Sprite sprite, ElementEnum elementEnum)
+    {
+        Sprite = sprite;
+        Element = elementEnum;
+    }
+}
